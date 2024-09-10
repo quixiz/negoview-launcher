edcd3dc9c4054086c8d822047dbb246f8d3d0eab
Maxime <maxime@quixiz.com>
2024-09-11 03:11:00 +0900

    Working auto updater

--- main.js
+++ main.js
@@ -25,7 +25,7 @@ function createWindow() {
     mainWindow.setAutoHideMenuBar(false);
 
     const webView = new WebContentsView();
-    webView.webContents.loadURL('https://www.google.com');
+    webView.webContents.loadURL('https://dev.negoview.com/provera5Mx/');
     mainWindow.contentView.addChildView(webView);
     webView.setBounds({ x: 0, y: 0, width: 0, height: 0 });
     mainWindow.on('resize', () => {
