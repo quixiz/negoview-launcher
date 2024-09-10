1a425bcf165613bae53b140a58f09e15c218f4eb
Maxime <maxime@quixiz.com>
2024-09-10 20:53:09 +0900

    Initial commit

--- preload.js
+++ preload.js
@@ -6,13 +6,3 @@
  *
  * https://www.electronjs.org/docs/latest/tutorial/sandbox
  */
-window.addEventListener('DOMContentLoaded', () => {
-  const replaceText = (selector, text) => {
-    const element = document.getElementById(selector)
-    if (element) element.innerText = text
-  }
-
-  for (const type of ['chrome', 'node', 'electron']) {
-    replaceText(`${type}-version`, process.versions[type])
-  }
-})
